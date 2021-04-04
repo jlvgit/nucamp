67687d2e0ea461ed64d462a44ba41ab478d9c31d
jlvgit <jlvel44@gmail.com>
2021-04-04 13:43:48 -0400

    Styling

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -1,20 +1,28 @@
-import React, { Component } from 'react';
-import { Navbar, NavbarBrand } from 'reactstrap';
-import './App.css';
+import React, { Component } from "react";
+import { Navbar, NavbarBrand } from "reactstrap";
+import "./App.css";
+import { CAMPSITES } from "./shared/campsites";
 
 // Components
-import Directory from './components/DirectoryComponent';
+import Directory from "./components/DirectoryComponent";
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            campsites: CAMPSITES,
+        };
+    }
+
     render() {
         return (
             <div className="App">
                 <Navbar dark color="primary">
-                <div className="container">
-                    <NavbarBrand href="/">NuCamp</NavbarBrand>
-                </div>
+                    <div className="container">
+                        <NavbarBrand href="/">NuCamp</NavbarBrand>
+                    </div>
                 </Navbar>
-                <Directory />
+                <Directory campsites={this.state.campsites} />
             </div>
         );
     }
