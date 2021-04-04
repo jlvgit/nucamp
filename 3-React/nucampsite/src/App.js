6a988c97d17e65a230e6605b648c221aa2b2f862
jlvgit <jlvel44@gmail.com>
2021-04-04 12:52:23 -0400

    directory component

--- 3-React/nucampsite/src/App.js
+++ 3-React/nucampsite/src/App.js
@@ -2,6 +2,9 @@ import React, { Component } from 'react';
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
 
+// Components
+import Directory from './components/DirectoryComponent';
+
 class App extends Component {
     render() {
         return (
@@ -11,6 +14,7 @@ class App extends Component {
                     <NavbarBrand href="/">NuCamp</NavbarBrand>
                 </div>
                 </Navbar>
+                <Directory />
             </div>
         );
     }
