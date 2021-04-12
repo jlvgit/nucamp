d237b2d0c6e27eadbdade302534909a8dece3238
jlvgit <jlvel44@gmail.com>
2021-04-12 19:22:27 -0400

    Adding to the Single Page App

--- 3-React/nucampsite/src/components/FooterComponent.js
+++ 3-React/nucampsite/src/components/FooterComponent.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 function Footer(props) {
     return (
@@ -9,62 +10,41 @@ function Footer(props) {
                         <h5>Links</h5>
                         <ul className="list-unstyled">
                             <li>
-                                <a href="#">Home</a>
+                                <Link to="/home">Home</Link>
                             </li>
                             <li>
-                                <a href="#">Directory</a>
+                                <Link to="/directory">Directory</Link>
                             </li>
                             <li>
-                                <a href="#">About</a>
+                                <Link to="/aboutus">About</Link>
                             </li>
                             <li>
-                                <a href="#">Contact</a>
+                                <Link to="/contactus">Contact</Link>
                             </li>
                         </ul>
                     </div>
                     <div className="col-6 col-sm-3 text-center">
                         <h5>Social</h5>
-                        <a
-                            className="btn btn-social-icon btn-instagram"
-                            href="http://instagram.com/"
-                        >
+                        <a className="btn btn-social-icon btn-instagram" href="http://instagram.com/">
                             <i className="fa fa-instagram" />
                         </a>{" "}
-                        <a
-                            className="btn btn-social-icon btn-facebook"
-                            href="http://www.facebook.com/"
-                        >
+                        <a className="btn btn-social-icon btn-facebook" href="http://www.facebook.com/">
                             <i className="fa fa-facebook" />
                         </a>{" "}
-                        <a
-                            className="btn btn-social-icon btn-twitter"
-                            href="http://twitter.com/"
-                        >
+                        <a className="btn btn-social-icon btn-twitter" href="http://twitter.com/">
                             <i className="fa fa-twitter" />
                         </a>{" "}
-                        <a
-                            className="btn btn-social-icon btn-google"
-                            href="http://youtube.com/"
-                        >
+                        <a className="btn btn-social-icon btn-google" href="http://youtube.com/">
                             <i className="fa fa-youtube" />
                         </a>
                     </div>
                     <div className="col-sm-4 text-center">
-                        <a
-                            role="button"
-                            className="btn btn-link"
-                            href="tel:+12065551234"
-                        >
+                        <a role="button" className="btn btn-link" href="tel:+12065551234">
                             <i className="fa fa-phone" /> 1-206-555-1234
                         </a>
                         <br />
-                        <a
-                            role="button"
-                            className="btn btn-link"
-                            href="mailto:notreal@notreal.co"
-                        >
-                            <i className="fa fa-envelope-o" />{" "}
-                            campsites@nucamp.co
+                        <a role="button" className="btn btn-link" href="mailto:notreal@notreal.co">
+                            <i className="fa fa-envelope-o" /> campsites@nucamp.co
                         </a>
                     </div>
                 </div>
