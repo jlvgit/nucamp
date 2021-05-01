39f8233118f79c3b1c8c051728ee98870aee16eb
jlvgit <jlvel44@gmail.com>
2021-05-01 11:47:33 -0400

    Adding post feedback to server

--- 3-React/nucampsite/src/components/HomeComponent.js
+++ 3-React/nucampsite/src/components/HomeComponent.js
@@ -47,7 +47,7 @@ function Home(props) {
 					/>
 				</div>
 				<div className="col-md m-1">
-					<RenderCard item={props.partner} />
+					<RenderCard item={props.partner} isLoading={props.partnerLoading} errMess={props.partnerErrMess} />
 				</div>
 			</div>
 		</div>
